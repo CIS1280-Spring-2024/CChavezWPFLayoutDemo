2eb5d25089de987b2f0620781fac070f6c269773
HitandFeyd <cchavez572@cnm.edu>
2024-02-29 18:08:11 -0700

    Word Doc implemented Commit

--- CChavezWPFLayoutDemo/MainWindow.xaml.cs
+++ CChavezWPFLayoutDemo/MainWindow.xaml.cs
@@ -20,5 +20,35 @@ namespace CChavezWPFLayoutDemo
         {
             InitializeComponent();
         }
+
+        private void btnStackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new StackPanelDemo();
+            window.Show();
+        }
+
+        private void btnWrapPannel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new WrapPanelDemo();
+            window.Show();
+        }
+
+        private void btnCanvasPannel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new CanvasDemo();
+            window.Show();
+        }
+
+        private void btnDockPannel_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new DockPanelDemo();
+            window.Show();
+        }
+
+        private void btnGridDemo_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new GridDemo();
+            window.Show();
+        }
     }
 }
